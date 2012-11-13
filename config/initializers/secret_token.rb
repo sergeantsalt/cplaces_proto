91def93b88a590b02f82d1cfda3a7bcaa7c048d4
Alex Nelson <sergeantsalt@gmail.com>
2012-11-12 20:53:33 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CplacesProto::Application.config.secret_token = 'e8f4a82a55923e901b313e8f4f06cb1afa4a1e93b292d9bf6eb9f35689ef42df91b31f0b8e18b592fcd16fd4f08674eec50c600bfc15dc433e7d1760aa1a6b07'
